091ba95040dbff593b587be19aabc5f5ac1dc811
@subekshya <>
2023-06-09 20:38:37 +0545

    Login form for RMS

---
+++ src/database/MyConnection.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author 97798
+ */
+public class MyConnection {
+        public static Connection doConnect(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rms","root","subekshya");
+            
+            System.out.println("connected"); 
+            return conn;
+        }
+        
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
---
+++ src/rms/RMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rms;
+
+/**
+ *
+ * @author 97798
+ */
+public class RMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
