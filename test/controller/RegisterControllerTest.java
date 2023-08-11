831d38c84838a99a45f8dca1744520b9c04f2601
Subekshya8686 <110591297+Subekshya8686@users.noreply.github.com>
2023-08-11 14:59:10 +0545

    Test (#28)
    * New
    
    * testing
    
    ---------
    
    Co-authored-by: sahilsheestha <sahilshrestha5309@gmail.com>

--- test/controller/RegisterControllerTest.java
+++ test/controller/RegisterControllerTest.java
@@ -1,49 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package controller;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import java.awt.event.ActionListener;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import controller.RegisterController;
+import static org.junit.Assert.*;
 import model.RegisterModel;
 import view.SignUp;
-import static org.junit.Assert.*;
 
-/**
- *
- * @author 97798
- */
 public class RegisterControllerTest {
-    
-    public RegisterControllerTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
+    private RegisterController controller;
+    private SignUpStub viewStub;
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        viewStub = new SignUpStub();
+        controller = new RegisterController(viewStub);
     }
 
     @Test
-    public void testSomeMethod() throws Exception {
-        // TODO review the generated test code and remove the default call to fail.
-        RegisterController controller = new RegisterController(new SignUp());
-        Boolean status = controller.checkUser(new RegisterModel("Ram", "Dangol", "ram@gmail.com", "9800000", "user1","123455","What is your name?","Ram"));
-        assertEquals(true, status);
-    }    
+    public void testCheckUser_ValidData_ReturnsTrue() throws Exception {
+        // Arrange: Prepare the test scenario
+        RegisterModel testUserData = new RegisterModel("Ram", "Dangol", "ram@gmail.com", "9800000", "user1","123455","What is your name?","Ram");
+        
+        // Set up the stub to provide test data
+        viewStub.setUser(testUserData);
+        
+        // Act: Call the method to be tested
+        boolean status = controller.checkUser(testUserData);
+
+        // Assert: Verify the result
+        assertTrue(status);
+    }
+
+    @Test
+    public void testCheckUser_InvalidData_ReturnsFalse() throws Exception {
+        // Arrange: Prepare the test scenario
+        RegisterModel testUserData = new RegisterModel("Invalid", "Data", "invalid@example.com", "123456", "invalid_user", "password", "Security Question?", "Answer");
+
+        // Set up the stub to provide test data
+        viewStub.setUser(testUserData);
+        
+        // Act: Call the method to be tested
+        boolean status = controller.checkUser(testUserData);
+
+        // Assert: Verify the result
+        assertFalse(status);
+    }
+    
+    // Stub implementation of the SignUp view class
+    private class SignUpStub extends SignUp {
+        private RegisterModel user;
+
+        @Override
+        public void addLoginListener(ActionListener listener) {
+            // Do nothing for the test
+        }
+
+        @Override
+        public RegisterModel getUser() {
+            return user;
+        }
+
+        @Override
+        public void setMessage(String message) {
+            // Do nothing for the test
+        }
+
+        public void setUser(RegisterModel user) {
+            this.user = user;
+        }
+    }
 }
