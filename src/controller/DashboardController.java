ee388a24fccebb505c1c0c9ec4bb5230b8506f16
Subekshya8686 <110591297+Subekshya8686@users.noreply.github.com>
2023-08-14 16:18:57 +0545

    Controller (#30)
    * controller
    
    * controller
    
    * controller
    
    * Updated
    
    ---------
    
    Co-authored-by: @subekshya <>

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -5,6 +5,7 @@
 package controller;
 
 import view.NewDashboard;
+import view.staff;
 import model.DashboardModel;
 
 public class DashboardController {
@@ -15,24 +16,35 @@ public class DashboardController {
        this.view = view;
        
    }
-//    
-//   public void totalbutton(DashboardModel model, NewDashboard view ){
-//       double tax = model.getTax();
-//       double total = model.getTotal();
-//       try{
-//           model = view.getTotal(); 
-////           System.out.println(getTax(tax));
-//        double taxAmount = view.getTax(tax);  
-////        double Total = view.gettxt();
-//        String formattedTax = String.format("%.0f", taxAmount);  
-//        System.out.println(formattedTax);  // Output: 12 (without decimal value)
-//        txtTax.setText(String.valueOf(formattedTax));
-//        txtAmount.setText(String.valueOf(total));
-//        txtTotal.setText(String.valueOf(total+ taxAmount));
-//    
-//       }
-//       catch(){
-//           
-//       }
-   }
+
+
+      public void staffbtn(){
+       try{
+           staff s = new staff();
+           s.setVisible(true);
+       }
+       catch(Exception e){
+           
+       }
+    }} 
+    
+//    public void totalbutton(DashboardModel model, NewDashboard view ){
+//        double tax = model.getTax();
+//        double total = model.getTotal();
+//        try{
+//            model = view.getTotal(); 
+// //           System.out.println(getTax(tax));
+//         double taxAmount = view.getTax(tax);  
+// //        double Total = view.gettxt();
+//         String formattedTax = String.format("%.0f", taxAmount);  
+//         System.out.println(formattedTax);  // Output: 12 (without decimal value)
+//         txtTax.setText(String.valueOf(formattedTax));
+//         txtAmount.setText(String.valueOf(total));
+//         txtTotal.setText(String.valueOf(total+ taxAmount));
+    
+//        }
+//        catch(){
+           
+//        }
+   
    
